5c065a6a486b6f9a6f648365ffed25ad88ac2bfd
k-0107 <k.tkm0107@gmail.com>
2024-10-03 21:28:04 +0900

    ページ遷移をできるようにした

---
+++ lib/main.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  // 最初に表示するWidget
+  runApp(const MyTodoApp());
+}
+
+class MyTodoApp extends StatelessWidget {
+  const MyTodoApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // アプリ名
+      title: 'My Todo App',
+      theme: ThemeData(
+        // テーマカラー
+        primarySwatch: Colors.blue,
+      ),
+      // リスト一覧画面を表示
+      home: const TodoListPage(),
+    );
+  }
+}
+
+// リスト一覧画面用Widget
+class TodoListPage extends StatelessWidget {
+  const TodoListPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: const Center(
+        child: Text('リスト一覧画面'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // "push"で新規画面に遷移
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => const TodoAddPage(),
+            ),
+          );
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class TodoAddPage extends StatelessWidget {
+  const TodoAddPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: TextButton(
+          // ボタンをクリックした時の処理
+          onPressed: () {
+            // "pop"で前の画面に戻る
+            Navigator.pop(context);
+          },
+          child: const Text('リスト追加画面（クリックで戻る）'),
+        ),
+      ),
+    );
+  }
+}
